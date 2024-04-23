cffc921e9add7aafccdd7f3d8e1fb211d29ea069
AikoJu <julya.aono@sptech.school>
2024-04-23 14:41:59 -0300

    Front-End

---
+++ gourmet-inventory/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Rotas from "./routes"
+
+function App() {
+  return (
+    <>
+    <Rotas/>
+    <ToastContainer/>
+    </>
+);
+}
+export default App;
---
+++ gourmet-inventory/src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function Rotas() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+export default Rotas;

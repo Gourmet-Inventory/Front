b84f09fe99cccdbf3f0ab3eaa8e25e4293496c91
Mirella-Ot <mirella.vidal@sptech.school>
2024-04-30 13:25:32 -0300

    Continuação front end

--- gourmet-inventory/src/pages/home/Home.jsx
+++ gourmet-inventory/src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Home.module.css";
 import logo from "../../utils/assets/Possíveis Paletas (5) 1.svg";
 import NavBar from "../../components/navbar/NavBar";
+import imgValores1 from "../../utils/assets/Valor 1.svg"
+import imgValores2 from "../../utils/assets/Valor 2.svg"
+import imgValores3 from "../../utils/assets/Valor 3.svg" 
 
 const Home = () => {
     return (
@@ -10,14 +13,14 @@ const Home = () => {
         <div className = {styles["home"]}>     
 
             <div className={styles["logo"]}>
-                <span>Logo</span>
+                <div className={styles["banner"]}></div>
             </div>
 
             <div className={styles["tituloRodape"]}>
                 <h1>O seu novo ajudante na organização!</h1>
 
                 <div className={styles["textoRodape"]}>
-                    <p>Em um mundo onde a eficiência e precisão são a chave, o <span>Gourmet Invetory</span> surge como solução para gerenciar seu estoque. </p>
+                    <p>Em um mundo onde a eficiência e precisão são a chave, o <span>Gourmet Inventory</span> surge como solução para gerenciar seu estoque. </p>
 
                     <p><span>Projetado para simplificar e otimizar o controle de estoque</span>, nosso software combina funcionalidades intuitivas com algoritmos
                     de análise, <span>garantindo que você tenha sempre a informação ce rta ao seu alcance.</span></p>
@@ -31,7 +34,7 @@ const Home = () => {
 
                 <div className={styles["valoresTitulo"]}>
                     <h1>VALORES</h1>
-                    <p>Estes valores nos unem e guiam nossa jornada. Eles refletem quem somos e aspiramos ser, não apenas como equipe, mas como parte da comunidade.</p>
+                    <p>Estes valores nos unem e nos guiam em nossa jornada. Eles refletem em quem somos e quem aspiramos ser, não apenas como equipe, mas como parte da comunidade.</p>
                 </div>
 
                 <div className={styles["cardsTitulo"]}>
@@ -39,7 +42,13 @@ const Home = () => {
                     <h1 className={styles["tituloQual"]}>QUALIDADE</h1>
                     <h1 className={styles["tituloOrg"]}>ORGANIZAÇÃO</h1>
                     <h1 className={styles["tituloPrat"]}>PRATICIDADE</h1>
-                    
+
+                </div>
+
+                <div className={styles["imgValores"]}>
+                    <img src={imgValores1} />
+                    <img src={imgValores2} />
+                    <img src={imgValores3} />
                 </div>
                 
 
--- gourmet-inventory/src/pages/login/Login.jsx
+++ gourmet-inventory/src/pages/login/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Login.module.css";
-// import imgBack from "../../utils/assets/francisco-suarez-0EkWTSFXwCc-unsplash 1.svg"
+import imgBack from "../../"
 
 const Login = () => {
     return (
         <>
+            <img src={imgBack}  alt="" />
             <div className={styles ["form"]} >
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing eli
                     t. Sunt odit, ut vitae suscipit incidunt dolorum ullam recusandae tempora cumque corporis quidem repellendus quibusdam! Repellendus vitae accusantium aliquam. Similique, error voluptatum!</span>

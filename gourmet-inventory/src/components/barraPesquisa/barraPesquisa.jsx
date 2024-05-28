abde1257932b0bd1b155c731c45b863d425ce34b
Mirella-Ot <mirella.vidal@sptech.school>
2024-05-28 17:44:42 -0300

    Começo do sistema

---
+++ gourmet-inventory/src/components/barraPesquisa/barraPesquisa.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import styles from "./barraPesquisa.module.css";
+import imgLupa from "../../utils/assets/lupa 5.svg"
+import imgFiltro from "../../utils/assets/filtro.svg"
+
+const barraPesquisa = ({tituloPag}) => {
+    return (
+        <>
+        <div className={styles["pesquisa"]}>
+            <h1>{tituloPag}</h1>
+           <div className={styles["barraPesquisa"]}>
+
+                    <input type="text" placeholder="Pesquisar"/>
+                    <img src={imgLupa} />
+                    <img src={imgFiltro} />
+                
+           </div>
+        </div>
+        </>
+    )
+};
+
+export default barraPesquisa;
---
+++ gourmet-inventory/src/components/excluir/excluir.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./excluir.module.css";
+
+
+const excluir = () => {
+    return (
+        <>
+           <div className={styles["formDelete"]}>
+                <h1>OIIIII</h1>
+           </div>
+        </>
+    )
+};
+
+export default excluir;

f1e7c5f57e6c02a4c03d10634b5b078f3b35ed2d
Mirella-Ot <mirella.vidal@sptech.school>
2024-06-19 01:23:59 -0300

    Continuação página saída

--- gourmet-inventory/src/pages/saida/Saida.jsx
+++ gourmet-inventory/src/pages/saida/Saida.jsx
@@ -7,12 +7,16 @@ import imgFechar from "../../utils/assets/Fechar.svg";
 import { toast } from 'react-toastify';
 import MenuLateral from "../../components/menuLateral/MenuLateral";
 import ModalSaida from "../../components/modalSaida/modalSaida";
+import fechar from "../../utils/assets/Fechar.svg";
 import api from "../../api";
 
 function Saida() {
     const [pratos, setPratos] = useState([]);
     const [pratosSelecionados, setPratosSelecionados] = useState([]);
+    const [historicoAdicoes, setHistoricoAdicoes] = useState([]);
     const [openVizualizar, setOpenVizualizar] = useState(false);
+    const [modalPratos, setModalPratos] = useState([]);
+    const [dataRelatorio, setDataRelatorio] = useState("");
 
     const navigate = useNavigate();
 
@@ -22,7 +26,6 @@ function Saida() {
         })
         .then(response => {
             if (Array.isArray(response.data)) {
-                console.log(response.data);
                 setPratos(response.data);
             } else {
                 console.error('A resposta da API não é um array:', response.data);
@@ -35,17 +38,53 @@ function Saida() {
         });
     }, []);
 
+    const gerarRelatorio = () => {
+        const idPratoList = modalPratos.map(prato => prato.idPrato);
+        if (!dataRelatorio) {
+            toast.error("Por favor, selecione uma data.");
+            return;
+        }
+        api.post(`/relatorio/gerar?data=${dataRelatorio}`, idPratoList, {
+            headers: { 'Authorization': `Bearer ${localStorage.token}` }
+        }).then(response => {
+            if (response.status === 200) {
+                toast.success("Relatório gerado com sucesso!");
+            } else {
+                toast.error("Erro ao gerar relatório.");
+            }
+        }).catch(error => {
+            console.error('Erro ao gerar relatório:', error);
+            toast.error("Erro ao gerar relatório.");
+        });
+    };
+
     const adicionarPrato = (prato) => {
         setPratosSelecionados([...pratosSelecionados, prato]);
+        setHistoricoAdicoes([...historicoAdicoes, prato]);
+    };
+
+    const desfazerAdicao = () => {
+        if (historicoAdicoes.length > 0) {
+            const ultimoPratoAdicionado = historicoAdicoes[historicoAdicoes.length - 1];
+            const novosPratosSelecionados = pratosSelecionados.filter((prato, index) => index !== pratosSelecionados.lastIndexOf(ultimoPratoAdicionado));
+            setPratosSelecionados(novosPratosSelecionados);
+            setHistoricoAdicoes(historicoAdicoes.slice(0, -1));
+        }
     };
 
     const limparLista = () => {
         setPratosSelecionados([]);
+        setHistoricoAdicoes([]);
     };
 
     const removerPrato = (index) => {
         const novosPratos = pratosSelecionados.filter((_, i) => i !== index);
         setPratosSelecionados(novosPratos);
+    };
+
+    const finalizar = () => {
+        setModalPratos(pratosSelecionados);
+        setOpenVizualizar(true);
     };
 
     return (
@@ -60,7 +99,7 @@ function Saida() {
                 <div className={styles["formPratos"]}>
                     <div className={styles["form"]}>
                         {pratos.map(prato => (
-                            <div className={styles["card"]} key={prato.id}>
+                            <div className={styles["card"]} key={prato.idPrato}>
                                 <div className={styles["imgCard"]}>
                                     {/* Aqui você pode colocar a imagem do prato, se tiver */}
                                     {/* <img src={prato.imagem} alt={prato.nome} /> */}
@@ -79,18 +118,14 @@ function Saida() {
                     <div className={styles["saidaPratos"]}>
                         <div className={styles["saida"]}>
                             <div className={styles["tituloSaida"]}>
-                                <span>Quant</span>
                                 <span>Pratos</span>
+                                <button onClick={desfazerAdicao}>Desfazer</button>
                                 <button onClick={limparLista}>Limpar Lista</button>
                             </div>
                             <div className={styles["corpoSaida"]}>
                                 {pratosSelecionados.map((prato, index) => (
                                     <div className={styles["cardSaida"]} key={index}>
-                                        <div className={styles["quantPrato"]}>
-                                            <span>-</span>
-                                            <span>1</span>
-                                            <span>+</span>
-                                        </div>
+                                        <span>{prato.categoria}</span>
                                         <span>{prato.nome}</span>
                                         <img src={imgFechar} alt="Remover" onClick={() => removerPrato(index)} />
                                     </div>
@@ -98,12 +133,50 @@ function Saida() {
                             </div>
                         </div>
                         <div className={styles["botoes"]}>
-                            <button id={styles["buttonSaida"]} onClick={() => setOpenVizualizar(true)}>Finalizar</button>
-                            <button id={styles["buttonRelatorio"]} onClick={() => setOpenVizualizar(true)}>Relatório</button>
+                            <button id={styles["buttonSaida"]} onClick={finalizar}>Finalizar</button>
+                            <button id={styles["buttonRelatorio"]} onClick={finalizar}>Relatório</button>
                         </div>
                     </div>
 
-                    <ModalSaida isOpen={openVizualizar} setModalOpen={() => setOpenVizualizar(!openVizualizar)} tituloModal={"Revisão"}/>
+                    <ModalSaida
+                        isOpen={openVizualizar}
+                        setOpenVizualizar={() => setOpenVizualizar(!openVizualizar)}
+                    >
+                        <div className={styles["modal"]}>
+                            <div className={styles["tituloModal"]}>
+                                <div className={styles["titulos"]}>
+                                    <span id={styles["titulo"]}>Revisão</span>
+                                </div>
+                                <img src={fechar} onClick={() => setOpenVizualizar(false)} />
+                            </div>
+                            <div className={styles["data"]}>
+                                <span>Data:</span>
+                                <input
+                                    type="date"
+                                    value={dataRelatorio}
+                                    onChange={(e) => setDataRelatorio(e.target.value)}
+                                />
+                            </div>
+                            <div className={styles["corpoModal"]}>
+                                <div className={styles["tituloSaida"]}>
+                                    <span>Preço</span>
+                                    <span>Pratos</span>
+                                </div>
+                                {modalPratos.map((prato, index) => (
+                                    <div className={styles["cardSaida"]} key={index}>
+                                        <div className={styles["quantPrato"]}>
+                                            <span>{prato.preco}</span>
+                                        </div>
+                                        <span>{prato.nome}</span>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className={styles["buttonModal"]}>
+                                <button id={styles["finalizar"]} onClick={gerarRelatorio}>Finalizar</button>
+                                <button id={styles["cancelar"]} onClick={() => setOpenVizualizar(false)}>Cancelar</button>
+                            </div>
+                        </div>
+                    </ModalSaida>
                 </div>
             </div>
         </>

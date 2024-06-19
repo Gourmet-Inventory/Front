4341846e59dbff48adb091cb8f60f763dd4dbcf8
AikoJu <julya.aono@sptech.school>
2024-06-18 21:10:21 -0300

    fix bug

--- gourmet-inventory/src/pages/pratos/cadastrarPratos/CadastrarPratos.jsx
+++ gourmet-inventory/src/pages/pratos/cadastrarPratos/CadastrarPratos.jsx
@@ -150,18 +150,19 @@ function CadastrarPratos() {
                 </div>
                 <div className={styles["medidas"]}>
                     <h1>Ingredientes</h1>
-                    <div className={styles["input"]} id={styles["ingredienteMedida"]}>
+                    <div className={styles["input1"]} id={styles["ingredienteMedida"]}>
                         <span>Ingrediente:</span>
                         <SelectIngredientes onSelect={handleSelectIngrediente} />
                     </div>
                     <div className={styles["formMedidas"]}>
-                        <div className={styles["input"]}>
+                        <div className={styles["input1"]}>
                             <span>Valor Medida:</span>
                             <input type="text" value={valorMedida} onChange={(e) => setValorMedida(e.target.value)} />
                         </div>
-                        <div className={styles["input"]}>
+                        <div className={styles["input1"]}>
                             <span>Tipo Medida:</span>
                             <select value={tipoMedida} onChange={(e) => setTipoMedida(e.target.value)}>
+                                <option value="" defaultChecked >Selecione uma medida</option>
                                 <option value="GRAMAS">Gramas(gr)</option>
                                 <option value="UNIDADE">Unidade</option>
                                 </select>
